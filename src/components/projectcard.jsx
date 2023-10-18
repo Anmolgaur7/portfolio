2cb5a999cd0296e5e1bd2775d6d413ec0740e005
Anmol <anmolgaur26@gmail.com>
2023-10-18 23:52:48 +0530

    changes in portfolio

--- src/components/projectcard.jsx
+++ src/components/projectcard.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import {FaGithub} from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
-export default function projectcard(props) 
-{
+export default function projectcard(props) {
   return (
-    <div className=' border-2 w-[70vw] h-[50vh] border-red-500 rounded-md p-2 m-2 text-white overflow-scroll md:w-[40vw] h-[50vh] overflow-x-hidden   '>
-     <div className='flex justify-center items-center'>
-     <a href={props.plink}><FaGithub className='text-4xl text-center  hover:text-red-400'/></a>    
-    </div>   
-    <img src={props.pimage} className='mb-2 w-auto' alt="project"/>
-    <h1 className='text-2xl font-bold font-sans text-white '>{props.ptitle}</h1>
-    <h1 className='text-md font-semibold font-sans text-white'>{props.pdesc}</h1> 
-    </div>
+    <a href={`${props.plink}`}>
+      <div className=' border-2 w-[70vw] h-[50vh] border-red-500 rounded-md p-2 m-2 text-white overflow-scroll md:w-[40vw] h-[50vh] overflow-x-hidden   '>
+        <img src={props.pimage} className='mb-2 w-[40vw]' alt="project" />
+        <h1 className='text-2xl font-bold font-sans text-white mb-2 mt-2'>{props.ptitle}</h1>
+        <h1 className='text-md font-semibold font-sans text-white'>{props.pdesc}</h1>
+      </div>
+    </a>
   )
 }
